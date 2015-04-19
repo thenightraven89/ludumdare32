3fcbaf85963370ea8e196c4b81ab72a1ff9a899d
Bogdan Tanasoiu <bogdan.tana@gmail.com>
2015-04-19 08:45:46 +0300

    really slow progress. finished tower animations

---
+++ New Unity Project/Assets/LevelActivator.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelActivator : MonoBehaviour
+{
+    void OnTriggerExit(Collider other)
+    {
+        LevelManager.instance.RaiseLadder();
+    }
+}
--- New Unity Project/Assets/LevelManager.cs
+++ New Unity Project/Assets/LevelManager.cs
@@ -6,15 +6,20 @@ public class LevelManager : MonoBehaviour
     public static LevelManager instance;
 
     public GameObject towerLevel;
+    public GameObject ladder;
+    public GameObject altar;
 
     private int towerHeight;
 
-    private const int levelHeight = 15;
+    private const int levelHeight = 14;
+
+    public Material[] pieceMaterials;
 
     public enum LevelState
     {
         None,
-        Ascension,
+        TowerRaising,
+        LadderRaising,
         Preparation,
         Combat,
         End
@@ -30,29 +35,80 @@ public class LevelManager : MonoBehaviour
     // start with this level state
     public void Start()
     {
+        //Cursor.lockState = CursorLockMode.Locked;
+
         currentState = LevelState.None;
         towerHeight = -1;
-        RaiseLevel();
+        RaiseTowerLevel();
+    }
+
+    private const float altarRaiseTime = 5f;
+
+    public void RaiseAltar()
+    {
+        LeanTween.move(altar, new Vector3(0, towerHeight * levelHeight, 0), altarRaiseTime).setEase(LeanTweenType.easeInOutQuad);
     }
 
     // launched when combat with current wave has ended, or at the beginning of the game, will launch animation for raising one level
-    public void RaiseLevel()
+    public void RaiseTowerLevel()
     {
-        currentState = LevelState.Ascension;
+        currentState = LevelState.TowerRaising;
 
         towerHeight++;
 
         GameObject newTowerLevel = Instantiate(towerLevel) as GameObject;
         newTowerLevel.transform.position = new Vector3(0, towerHeight * levelHeight, 0);
-        newTowerLevel.transform.eulerAngles = new Vector3(0, - towerHeight * 90f, 0);
+        //newTowerLevel.transform.eulerAngles = new Vector3(0, -towerHeight * 180f, 0);
 
         Transform[] parts = newTowerLevel.GetComponentsInChildren<Transform>();
         
         StartCoroutine(Construct(new object[] { parts, newTowerLevel.transform.position }));
     }
 
+    public void RaiseLadder()
+    {
+        currentState = LevelState.LadderRaising;
+
+        GameObject newLadder = Instantiate(ladder) as GameObject;
+        newLadder.transform.position = new Vector3(0, towerHeight * levelHeight, 0);
+        //newLadder.transform.eulerAngles = new Vector3(0, -towerHeight * 180f, 0);
+
+        Transform[] parts = newLadder.GetComponentsInChildren<Transform>();
+
+        StartCoroutine(Raise(parts));
+    }
+
+    private IEnumerator Raise(Transform[] parts)
+    {
+        Vector3[] initialPositions = new Vector3[parts.Length];
+
+        for (int i = 0; i < parts.Length; i++)
+        {
+            if (parts[i].CompareTag("Constructed"))
+            {
+                initialPositions[i] = parts[i].localPosition;
+                parts[i].localPosition = parts[i].localPosition + new Vector3(0f, -100f, 0f);
+                parts[i].gameObject.SetActive(false);
+            }
+        }
+
+        for (int i = 0; i < parts.Length; i++)
+        {
+            if (parts[i].CompareTag("Constructed"))
+            {
+                yield return new WaitForSeconds(Random.Range(minWait, maxWait));
+                AssignRandomMaterials(parts[i]);
+                parts[i].gameObject.SetActive(true);
+                LeanTween.moveLocal(parts[i].gameObject, initialPositions[i], 2f).setEase(LeanTweenType.easeOutQuint);
+            }
+        }
+
+        RaiseTowerLevel();
+    }
+
+
     private const float minWait = 0.01f;
-    private const float maxWait = 0.1f;
+    private const float maxWait = 0.05f;
     
     private IEnumerator Construct(object[] parameters)
     {
@@ -69,7 +125,7 @@ public class LevelManager : MonoBehaviour
 
             Vector3 initialPosition = parts[i].position;
 
-            if (parts[i].tag == "Constructed")
+            if (parts[i].CompareTag("Constructed"))
             {
                 parts[i].position = initialPosition + direction * 100f;
                 parts[i].gameObject.SetActive(false);
@@ -79,12 +135,15 @@ public class LevelManager : MonoBehaviour
         for (int i = 0; i < parts.Length; i++)
         {
             yield return new WaitForSeconds(Random.Range(minWait, maxWait));
-            if (parts[i].tag == "Constructed")
+            if (parts[i].CompareTag("Constructed"))
             {
+                AssignRandomMaterials(parts[i]);
                 parts[i].gameObject.SetActive(true);
                 LeanTween.move(parts[i].gameObject, initialPositions[i], 0.5f).setEase(LeanTweenType.easeOutQuint);
             }
         }
+
+        RaiseAltar();
 
         yield return null;
     }
@@ -99,5 +158,14 @@ public class LevelManager : MonoBehaviour
     public void Fight()
     {
         currentState = LevelState.Combat;
+    }
+
+    private void AssignRandomMaterials(Transform target)
+    {
+        MeshRenderer mrenderer = target.GetComponent<MeshRenderer>();
+        if (mrenderer != null)
+        {
+            mrenderer.material = pieceMaterials[Random.Range(0, pieceMaterials.Length)];
+        }
     }
 }
