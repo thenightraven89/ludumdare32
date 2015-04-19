b204199eddfce8b725b0f8731b1387eafc801f1c
Bogdan Tanasoiu <bogdan.tana@gmail.com>
2015-04-19 18:35:10 +0300

    tweaked previous stuff, added first person object interaction. now we can play the flute!

--- New Unity Project/Assets/FlutePlayer.cs
+++ New Unity Project/Assets/FlutePlayer.cs
@@ -9,8 +9,15 @@ public class FlutePlayer : MonoBehaviour
 
     private Vector3 initialFlutePosition;
     private Vector3 initialFluteRotation;
-    
 
+    public KeyCode[] keycodes;
+    public AudioClip[] clips;
+
+    public AudioSource panfluteSource;
+
+    public Material highlightedMaterial;
+    private Material preHighlightMaterial;
+    
     // Use this for initialization
     void Start()
     {
@@ -18,19 +25,86 @@ public class FlutePlayer : MonoBehaviour
         initialFluteRotation = flute.transform.localEulerAngles;
     }
 
+    private LTDescr fluteDescr;
+
+    private GameObject selectedObject;
+    //private GameObject pHolder;
+
     // Update is called once per frame
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
+            if (fluteDescr != null)
+            {
+                LeanTween.cancel(flute, fluteDescr.uniqueId);
+            }
+
             LeanTween.moveLocal(flute, playingFlutePosition.transform.localPosition, 0.5f).setEase(LeanTweenType.easeInOutQuad);
             LeanTween.rotateLocal(flute, playingFlutePosition.transform.localEulerAngles, 0.5f).setEase(LeanTweenType.easeInOutQuad);
         }
 
         if (Input.GetMouseButtonUp(0))
         {
+            panfluteSource.Stop();
+
+            if (fluteDescr != null)
+            {
+                LeanTween.cancel(flute, fluteDescr.uniqueId);
+            }
+
             LeanTween.moveLocal(flute, initialFlutePosition, 0.5f).setEase(LeanTweenType.easeInOutQuad);
             LeanTween.rotateLocal(flute, initialFluteRotation, 0.5f).setEase(LeanTweenType.easeInOutQuad);
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            for (int i = 0; i < keycodes.Length; i++)
+            {
+                if (Input.GetKeyDown(keycodes[i]))
+                {
+                    panfluteSource.Stop();
+                    panfluteSource.PlayOneShot(clips[i]);
+                }
+
+                if (Input.GetKeyUp(keycodes[i]))
+                {
+                    panfluteSource.Stop();
+                }
+            }
+        }
+
+        Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.transform.gameObject.layer == LayerMask.NameToLayer("Interactable"))
+            {
+                DisposeOfSelectedObjectHighlight();
+
+                selectedObject = hit.transform.gameObject;
+                preHighlightMaterial = selectedObject.GetComponent<MeshRenderer>().material;
+                highlightedMaterial.color = preHighlightMaterial.color;
+                selectedObject.GetComponent<MeshRenderer>().material = new Material(highlightedMaterial);
+            }
+            else
+            {
+                DisposeOfSelectedObjectHighlight();
+            }
+        }
+        else
+        {
+            DisposeOfSelectedObjectHighlight();
+        }
+    }
+
+    private void DisposeOfSelectedObjectHighlight()
+    {
+        if (selectedObject != null)
+        {
+            selectedObject.GetComponent<MeshRenderer>().material = preHighlightMaterial;
+            preHighlightMaterial = null;
+            selectedObject = null;
         }
     }
 }
